dee37f6685ba3a998f23f596ad917982150e6261
Stijn van Wijngaarden <stijn@mriguidance.com>
2025-02-27 16:21:08 +0100

    Fix initializing the patient datastore

--- monailabel/datastore/patient.py
+++ monailabel/datastore/patient.py
@@ -25,12 +25,13 @@ class PatientDatastore(LocalDatastore):
 
     def _init_patient_mapping(self):
         # Group images by patient ID (assuming filenames start with patient ID)
-        for image_id in self.image_ids():
+        # Use list_images() instead of image_ids() - this is the method defined in LocalDatastore
+        for image_id in self.list_images():
             patient_id = image_id.split("_")[0]  # Assumes filename format: PATIENTID_*.nii.gz
             if patient_id not in self.patient_images:
                 self.patient_images[patient_id] = []
             self.patient_images[patient_id].append(image_id)
-        logger.info(f"Found {len(self.patient_images)} patients with {len(self.image_ids())} total images")
+        logger.info(f"Found {len(self.patient_images)} patients with {len(self.list_images())} total images")
 
     def get_patient_images(self, patient_id: str) -> List[str]:
         return self.patient_images.get(patient_id, [])
@@ -43,5 +44,25 @@ class PatientDatastore(LocalDatastore):
         patient_id = image_id.split("_")[0]
         if patient_id not in self.patient_images:
             self.patient_images[patient_id] = []
-        self.patient_images[patient_id].append(image_id) 
+        if image_id not in self.patient_images[patient_id]:
+            self.patient_images[patient_id].append(image_id)
         return image_id
+        
+    def remove_image(self, image_id: str) -> None:
+        # Override to maintain patient mapping when images are removed
+        patient_id = image_id.split("_")[0]
+        if patient_id in self.patient_images and image_id in self.patient_images[patient_id]:
+            self.patient_images[patient_id].remove(image_id)
+            # Remove patient entry if no more images
+            if not self.patient_images[patient_id]:
+                del self.patient_images[patient_id]
+                
+        # Call parent implementation to remove the image
+        super().remove_image(image_id)
+        
+    def refresh(self):
+        # Override refresh to update patient mapping
+        super().refresh()
+        # Rebuild patient mapping
+        self.patient_images.clear()
+        self._init_patient_mapping()
